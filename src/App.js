7713d5284a58920df3f174be9827c46f17fbabe8
Cirizz <b4304259@gmail.com>
2023-01-21 03:53:33 +0700

    add prettier and styled-components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,57 @@
 import React, { useState, useEffect, useCallback } from "react";
 import SwapiHero from "./components/SwapiHero";
-import axios from 'axios';
+import axios from "axios";
+import styled from "styled-components";
+const Input = styled.input`
+height: 25px;
+width: 250px;
+font-size: 20px;
+background-color: azure;
+border: 1px solid blueviolet;
+`
+function HomePage() {
+  // - компонент
+  const [input, setInput] = useState("");
+  const [hero, setHero] = useState(""); // для данных о герое
+  const [data, setData] = useState([]); // data - переменная состояния(начальное состояние), setData - функция изменения состояния
 
-function HomePage() { // - компонент
-  const [input, setInput] = useState('');
-  const [hero, setHero] = useState(''); // для данных о герое
-  const [data, setData] = useState([])// data - переменная состояния(начальное состояние), setData - функция изменения состояния
-  function makeSwapiRequests() { // как вариант обернуть в промис?????
+  function makeSwapiRequests() {
+    // как вариант обернуть в промис?????
     axios({
       method: "get",
       url: "https://swapi.dev/api/people/?search=" + input,
-    })
-      .then(res => setData(res.data.results))
+    }).then((res) => setData(res.data.results));
   }
-  useEffect(() => { //эффекты выполняются после каждого рендера, только после изменения DOM
-    makeSwapiRequests()
-  }, [input])
+  useEffect(() => {
+    //эффекты выполняются после каждого рендера, только после изменения DOM
+    makeSwapiRequests();
+  }, [input]);
   function searchHero(event) {
     const { value } = event.target;
     setInput(value.toLowerCase());
   }
-  function handleSwapiHero(name){ //обрабатываем клик
+  function handleSwapiHero(name) {
+    //обрабатываем клик
     setInput(name);
     const dataFind = data.find((element) => element.name === name); // ищем в массиве объект с нашим героем
     setHero(JSON.stringify(dataFind));
   }
   return (
     <div className="App">
-      <div><input onChange={searchHero}  placeholder="Hero name:" /></div>
-      {data
-        .map(({ name }) => {
-          return (
-            <SwapiHero onClick={() => handleSwapiHero(name)} key={name} name={name}></SwapiHero>
-          )
-        })}
-        <div>{hero}</div>
-    </div>)
-};
+      <div>
+        <Input onChange={searchHero} placeholder="Hero name:" />
+      </div>
+      {data.map(({ name }) => {
+        return (
+          <SwapiHero
+            onClick={() => handleSwapiHero(name)}
+            key={name}
+            name={name}
+          ></SwapiHero>
+        );
+      })}
+      <div>{hero}</div>
+    </div>
+  );
+}
 export default HomePage;
-
--- src/components/SwapiHero.js
+++ src/components/SwapiHero.js
@@ -1,21 +1,36 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+const DivesCont=styled.div`
+font-size: 15px;
+background-color: plum;
+color: indigo;
+width: 254px;
+border: 1px solid darkcyan;
+`;
+const Names=styled.b`
+  font-size: 20px;
+  color: blue;
+  `;
 
-function SwapiHero({ name, onClick }) {
-    // console.log('[SwapiHero] render')
-    return <div onClick={onClick}>
-        <div>Name: <b style={{ color: 'blue' }}>{name}</b></div>
-    </div>
-};
+function SwapiHero({ name, onClick }) { 
+  return (
+    <DivesCont onClick={onClick}>
+      <div>
+        Name: <Names>{name}</Names>
+      </div>
+    </DivesCont>
+  );
+}
 
 SwapiHero.propTypes = {
-    onClick: PropTypes.func,
-    name: PropTypes.string
+  onClick: PropTypes.func,
+  name: PropTypes.string,
 };
 
 SwapiHero.defaultProps = {
-    onClick: () => {},
-    name: 'goida'
+  onClick: () => {},
+  name: "goida",
 };
 
 export default SwapiHero;
